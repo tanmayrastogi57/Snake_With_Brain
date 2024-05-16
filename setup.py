9538c860352afa3cdf6674b42b814b5ca49921c4
tanmayrastogi57 <tanmayrastogi57@gmail.com>
2024-05-17 00:10:18 +0530

    Tanmay Rastogi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='snake',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Tanmay_Rastogi',
+    author_email='tanmayrastogi57@gmail.com',
+    description='An Deep learning arcade snake model that learns by it self using Deep Q learning and Neural Network.'
+)
